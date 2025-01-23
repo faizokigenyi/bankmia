00bc8d062e33f5bab7722fa41f122cb5708a4c51
Faizo Kigenyi <faizokigenyi@gmail.com>
2025-01-22 21:32:36 -0600

    add chart, button, and sheet  fron shadcn

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import SideBar from "../../../components/ui/SideBar";
+import MobileNav from "../../../components/ui/MobileNav";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggedIn = {
+    firstName: "Mia",
+    lastName: "Gonzales",
+  };
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+      <div className="w-full flex flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="menu icon" width={30} height={30} />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
+}
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HeaderBox from "../../../components/ui/HeaderBox";
+import TotalBalanceBox from "../../../components/ui/TotalBalanceBox";
+
+const Home = () => {
+  const loggedIn = {
+    firstName: "Gonzales",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="welcome"
+          user={loggedIn?.firstName || " Guest  "}
+          subtext=" Access and manage your accounts and transactions effortlessly."
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250 - 35}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
